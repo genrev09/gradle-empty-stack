053825ded8eb7af0d889f848c8d3f812d6fd6f8c
GENREV ARAMBULO (SAPE-MIS-ISD-OOCLL/MNL) <ARAMBGE@OOCL.COM>
2019-10-10 13:48:38 +0800

    Added validation if message exceed length for Android

--- src/main/java/Android.java
+++ src/main/java/Android.java
@@ -1,6 +1,11 @@
 public class Android extends Mobile{
+    private final int messageLimit = 15;
     @Override
     public void call(String message) {
-        System.out.println("<Android>Message : "+message);
+        if(checkMessageExceedLength(message,messageLimit))
+            System.out.println("<Android> Message cannot be sent");
+        else
+            System.out.println("<Android>Message : "+message);
+
     }
 }
