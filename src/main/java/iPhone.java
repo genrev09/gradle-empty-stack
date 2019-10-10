b4f1916280123799daba0df7de0e80e9a088b362
GENREV ARAMBULO (SAPE-MIS-ISD-OOCLL/MNL) <ARAMBGE@OOCL.COM>
2019-10-10 13:48:57 +0800

    Added validation if message exceed length for iPhone

--- src/main/java/iPhone.java
+++ src/main/java/iPhone.java
@@ -1,6 +1,10 @@
 public class iPhone extends Mobile{
+    private final int messageLimit = 25;
     @Override
     public void call(String message) {
-        System.out.println("<iPhone>Message : "+message);
+        if(checkMessageExceedLength(message,messageLimit))
+            System.out.println("<iPhone> Message cannot be sent");
+        else
+            System.out.println("<iPhone>Message : "+message);
     }
 }
